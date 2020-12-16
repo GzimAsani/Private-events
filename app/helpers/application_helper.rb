17c2a01a22b4c17d6323ddde11d6e97e54276416
Gzim <gzimasanni@gmail.com>
2020-12-16 14:51:00 +0100

    Style new events page and show events page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,12 @@ module ApplicationHelper
   def display_buttons
     buttons = []
     if logged_in?
-      buttons.push(link_to(current_user.username, user_path(current_user),
-                           class: 'text-light mr-2 bg-success py-1 px-3'))
-      buttons.push(link_to('Create Event', new_event_path,
-                           class: 'text-light mr-2 bg-success py-1 px-3'))
-      buttons.push(link_to('Logout', logout_path, method: :delete,
-                                                  class: 'text-light mr-2 bg-success py-1 px-3'))
+      buttons.push(link_to(current_user.username, user_path(current_user), class: 'text-light mr-2 bg-success py-1 px-3'))
+      buttons.push(link_to('Create Event', new_event_path, class: 'text-light mr-2 bg-success py-1 px-3'))
+      buttons.push(link_to('Logout', logout_path, method: :delete, class: 'text-light mr-2 bg-success py-1 px-3'))
     else
-      buttons.push(link_to('Login', login_path,
-                           class: 'text-light mr-2 bg-success py-1 px-3'))
-      buttons.push(link_to('Sign Up', new_user_path,
-                           class: 'text-light mr-2 bg-success py-1 px-3'))
+      buttons.push(link_to('Login', login_path, class: 'text-light mr-2 bg-success py-1 px-3'))
+      buttons.push(link_to('Sign Up', new_user_path, class: 'text-light mr-2 bg-success py-1 px-3'))
     end
     buttons
   end
@@ -21,16 +16,11 @@ module ApplicationHelper
     buttons = []
     if events_created.find_by(id: :event_id)
       buttons.push(link_to('Enroll',
-                           attendances_path(controller: 'attendances', action: 'create',
-                                            event_id: event_id), method: :post, class: 'btn btn-success'))
-      buttons.push(link_to('cancel', attendance_path(event_id), method: :delete,
-                                                                class: 'btn btn-danger mt-1'))
+                           attendances_path(controller: 'attendances', action: 'create', event_id: event_id), method: :post, class: 'btn btn-outline-success'))
+      buttons.push(link_to('cancel', attendance_path(event_id), method: :delete, class: 'btn btn-outline-danger mt-1'))
     else
-      buttons.push(link_to('Enroll',
-                           attendances_path(controller: 'invites', action: 'create',
-                                            event_id: event_id), method: :post, class: 'btn btn-success'))
-      buttons.push(link_to('Cancel', attendance_path(event_id), method: :delete,
-                                                                class: 'btn btn-danger mt-1'))
+      buttons.push(link_to('Enroll', attendances_path(controller: 'invites', action: 'create', event_id: event_id), method: :post, class: 'btn btn-outline-success'))
+      buttons.push(link_to('Cancel', attendance_path(event_id), method: :delete, class: 'btn btn-outline-danger mt-1'))
     end
     buttons
   end
